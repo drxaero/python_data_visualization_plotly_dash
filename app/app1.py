drxaero/python_data_visualization_plotly_dash

commit fb1296eb90f9255635fea29c2adce7a14a3f488c

BUG_FIXED - Make the `app1` Single-Page-Application by setting the Container.fluid=True. BUG_FIXED - Added executable attribute to `start-app.py`.

app/app1.py
@@ -226,6 +226,7 @@ app1.layout = dbc.Container(
             ),
         ),
     ],
+    fluid=True,
 )
 
 

commit 8a59b4e8b1c0915b92d0275e128dcba51d8bbafe

BUG_FIXED - Fixed the bug introduced by commit fb1296eb that there are spaces at both start and end of nav bar items at lg screens. BUG_FIXED - Now there're no paddings at both start and end of nav bar.

app/app1.py
@@ -76,7 +76,13 @@ nav_bar = dbc.Navbar(
                 children=[
                     dbc.Nav(
                         children=[
-                            dbc.NavItem(dbc.NavLink("CV", href="https://www.linkedin.com/in/januscheng/")),
+                            dbc.NavItem(
+                                dbc.NavLink(
+                                    "CV",
+                                    href="https://www.linkedin.com/in/januscheng/",
+                                    external_link=True,
+                                )
+                            ),
                         ],
                         navbar=True,
                         class_name="me-auto",
@@ -87,10 +93,11 @@ nav_bar = dbc.Navbar(
                 navbar=True,
             ),
         ],
+        fluid=True,
     ),
     id=nav_bar_id,
     sticky="top",
-    class_name="navbar-expand mb-1",
+    class_name="navbar-expand mb-1 py-0",
 )
 
 
@@ -224,9 +231,11 @@ app1.layout = dbc.Container(
                     ),
                 ],
             ),
+            class_name="px-3",
         ),
     ],
     fluid=True,
+    class_name="gx-0",  # ensuring no paddings at both start and end of nav-bar
 )
 
 